cc2309927bb0b1727fa49775a5cf8f37024ffa31
Taylor Spencer <tspencer@nextcenturymeters.com>
2021-12-03 15:14:23 -0700

    Adding in flash peripheral placeholder

--- src/mcu/flash.rs
+++ src/mcu/flash.rs
@@ -1,22 +1,21 @@
 //==============================================================================
 // Notes
 //==============================================================================
-// mcu/mod.rs
+// mcu/flash.rs
+
+/*
+ * Flash is arranged in 128 pages of 2k bytes: 256K total
+ * 128k at address 0x0000_0000	- 	0x0003_FFFF
+ * 128k at address 0x0800_0000 	- 	0x0803_FFFF
+ */
 
 //==============================================================================
 // Crates and Mods
 //==============================================================================
+use core::cell::RefCell;
+// use core::ops::DerefMut;
+use cortex_m::interrupt::{free, Mutex};
 use stm32f3::stm32f303;
-
-pub mod adc;
-pub mod flash;
-pub mod gpio;
-pub mod i2c;
-pub mod rtc;
-pub mod spi;
-pub mod timer;
-pub mod uart;
-pub mod wdt;
 
 //==============================================================================
 // Enums, Structs, and Types
@@ -26,65 +25,14 @@ pub mod wdt;
 //==============================================================================
 // Variables
 //==============================================================================
-
+static FLASH_HANDLE: Mutex<RefCell<Option<stm32f303::FLASH>>> = 
+	Mutex::new(RefCell::new(None));
 
 //==============================================================================
 // Public Functions
 //==============================================================================
-pub fn init() {
-	let peripherals = stm32f303::Peripherals::take().unwrap();
-	
-	adc::init(
-		peripherals.ADC1,
-		peripherals.ADC2,
-		peripherals.ADC3,
-		peripherals.ADC4
-	);
-	flash::init(
-		peripherals.FLASH
-	);
-	gpio::init(
-		peripherals.GPIOA,
-		peripherals.GPIOB,
-		peripherals.GPIOC,
-		peripherals.GPIOD,
-		peripherals.GPIOE,
-		peripherals.GPIOF,
-		peripherals.GPIOG,
-		peripherals.GPIOH
-	);
-	i2c::init(
-		peripherals.I2C1,
-		peripherals.I2C2,
-		peripherals.I2C3
-	);
-	rtc::init(
-		peripherals.RTC
-	);
-	spi::init(
-		peripherals.SPI1,
-		peripherals.SPI2,
-		peripherals.SPI3,
-		peripherals.SPI4
-	);
-	timer::init(
-		peripherals.TIM1,
-		peripherals.TIM8,
-		peripherals.TIM6,
-		peripherals.TIM7,
-		peripherals.TIM2,
-		peripherals.TIM3,
-		peripherals.TIM4,
-		peripherals.TIM15,
-		peripherals.TIM16,
-		peripherals.TIM17
-	);
-	uart::init(
-		peripherals.USART1,
-		peripherals.USART2,
-		peripherals.USART3
-	);
-	wdt::init(peripherals.IWDG);
+pub fn init(flash: stm32f303::FLASH) {
+	free(|cs| FLASH_HANDLE.borrow(cs).replace(Some(flash)));
 }
 
 //==============================================================================
@@ -96,13 +44,5 @@ pub fn init() {
 // Task Handler
 //==============================================================================
 pub fn task_handler() {
-	adc::task_handler();
-	flash::task_handler();
-	gpio::task_handler();
-	i2c::task_handler();
-	rtc::task_handler();
-	spi::task_handler();
-	timer::task_handler();
-	uart::task_handler();
-	wdt::task_handler();
+
 }
